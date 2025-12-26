4a5cdf07e1515bfd6bff23e56e392de2ecd8be41
zidanali111 <zidanali111@gmail.com>
2025-12-26 22:11:20 +0530

    project update v1.0.3

--- src/main/java/com/zidtech/common/security/config/SecurityAutoConfiguration.java
+++ src/main/java/com/zidtech/common/security/config/SecurityAutoConfiguration.java
@@ -1,5 +1,7 @@
 package com.zidtech.common.security.config;
 
+import com.zidtech.common.security.filter.JwtAuthenticationFilter;
+import com.zidtech.common.security.service.SecurityUserService;
 import com.zidtech.common.security.util.JwtUtil;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,5 +14,14 @@ public class SecurityAutoConfiguration {
     @Bean
     public JwtUtil jwtUtil(SecurityProperties properties) {
         return new JwtUtil(properties);
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(
+            JwtUtil jwtUtil,
+            SecurityUserService userService,
+            SecurityProperties properties
+    ) {
+        return new JwtAuthenticationFilter(jwtUtil, userService, properties);
     }
 }
